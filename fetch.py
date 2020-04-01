eb1a630ec5e4be088c68b8d6417d0986a525cb30
Jan Hoersch <jhoersch@gmail.com>
2020-04-01 12:32:55 +0200

    call plugins directly to check without querying a lot of pages

--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import importlib
 import pandas as pd
 
@@ -7,6 +8,14 @@ if __name__ == '__main__':
 
     modules = [os.path.splitext(f)[0] for f in os.listdir('plugins') if os.path.isfile(os.path.join('plugins',f))]
     
+    if len(sys.argv) > 1:
+        df = pd.DataFrame()
+        m = importlib.import_module('plugins.'+sys.argv[1])
+        df = m.run()
+
+        print(df)
+        sys.exit(0)
+
     for module in modules:
         df = pd.DataFrame()
         m = importlib.import_module('plugins.'+module)
