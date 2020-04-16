17b932d50987d0c28e44e33de46e82c8dca4d341
Jan Hoersch <jhoersch@gmail.com>
2020-04-16 22:24:11 +0200

    rp made some strange stuff

--- plugins/rp.py
+++ plugins/rp.py
@@ -5,14 +5,17 @@ def run():
     config = {   
         'name': 'Rheinland-Pfalz',
         'url': 'https://msagd.rlp.de/de/unsere-themen/gesundheit-und-pflege/gesundheitliche-versorgung/oeffentlicher-gesundheitsdienst-hygiene-und-infektionsschutz/infektionsschutz/informationen-zum-coronavirus-sars-cov-2/',
-        'table': 0
+        'table': 0,
+        'remove_columns':[3,4]
     }
-    
     df = pd.DataFrame()
 
     df = pd.read_html(config['url'],decimal=',', thousands='.')[int(config['table'])]
+    if 'remove_columns' in config:
+        df = df.drop(df.columns[config['remove_columns']],axis=1)
+    df = df[df[1] != 'Stadt']
     df = df.drop_duplicates(1,keep=False) # strange tables with lk and town splitted
-    df = df[:-1] # remove last line, because it is just time information
+    df = df[1:-1] # remove last line, because it is just time information
     df.insert(2,'inzidenz','',True)
     df.columns = ['Landkreis','Faelle','Inzidenz','Todesfaelle']
     df['Faelle'] = df['Faelle'].astype(str).str.split(n=1,expand=True)
